88a7e88412d87d43e2f21635e162fdff6d323677
malinaNata <kalinka007@tut.by>
2019-05-19 17:12:49 +0300

    feat days in timer

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -1998,15 +1998,17 @@ module.exports = tabs;
 /***/ (function(module, exports) {
 
 function timer() {
-  var deadline = '2019-05-22';
+  var deadline = '2019-05-25';
 
   function getTimeRemaining(endtime) {
     var t = Date.parse(endtime) - Date.parse(new Date()),
         seconds = Math.floor(t / 1000 % 60),
         minutes = Math.floor(t / 1000 / 60 % 60),
-        hours = Math.floor(t / (1000 * 60 * 60));
+        hours = Math.floor(t / 1000 / 60 / 60 % 24),
+        days = Math.floor(t / (1000 * 60 * 60 * 24));
     return {
       'total': t,
+      'days': days,
       'hours': hours,
       'minutes': minutes,
       'seconds': seconds
@@ -2015,6 +2017,7 @@ function timer() {
 
   function setClock(id, endtime) {
     var timer = document.getElementById(id),
+        days = timer.querySelector('.days'),
         hours = timer.querySelector('.hours'),
         minutes = timer.querySelector('.minutes'),
         seconds = timer.querySelector('.seconds'),
@@ -2025,10 +2028,12 @@ function timer() {
 
       if (t.total <= 0) {
         clearInterval(timeInterval);
+        days.textContent = '0 дней';
         hours.textContent = '00';
         minutes.textContent = '00';
         seconds.textContent = '00';
       } else {
+        days.textContent = text(t.days);
         hours.textContent = addZero(t.hours);
         minutes.textContent = addZero(t.minutes);
         seconds.textContent = addZero(t.seconds);
@@ -2040,6 +2045,20 @@ function timer() {
         return '0' + num;
       } else {
         return num;
+      }
+    }
+
+    function text(num) {
+      var count = num % 10;
+
+      if (count == 1) {
+        return num + ' день';
+      }
+
+      if (count > 1 && count < 5) {
+        return num + ' дня';
+      } else {
+        return num + ' дней';
       }
     }
   }
--- src/js/parts/timer.js
+++ src/js/parts/timer.js
@@ -1,14 +1,16 @@
 function timer() {
-    let deadline = '2019-05-22';
+    let deadline = '2019-05-25';
 
     function getTimeRemaining(endtime) {
         let t = Date.parse(endtime) - Date.parse(new Date()),
             seconds = Math.floor((t/1000) % 60),
             minutes = Math.floor((t/1000/60)%60),
-            hours = Math.floor((t/(1000*60*60)));
+            hours = Math.floor((t/1000/60/60) % 24),
+            days = Math.floor((t/(1000*60*60*24)));
 
         return {
             'total' : t,
+            'days' : days,
             'hours' : hours,
             'minutes' : minutes,
             'seconds' : seconds
@@ -17,6 +19,7 @@ function timer() {
 
     function setClock(id, endtime) {
         let timer = document.getElementById(id),
+            days = timer.querySelector('.days'),
             hours = timer.querySelector('.hours'),
             minutes = timer.querySelector('.minutes'),
             seconds = timer.querySelector('.seconds'),
@@ -27,20 +30,34 @@ function timer() {
 
             if (t.total <= 0) {
                 clearInterval(timeInterval);
+                days.textContent = '0 дней';
                 hours.textContent = '00';
                 minutes.textContent = '00';
                 seconds.textContent = '00';
             } else {
+                days.textContent = text(t.days);
                 hours.textContent = addZero(t.hours);
                 minutes.textContent = addZero(t.minutes);
                 seconds.textContent = addZero(t.seconds);
             }
         }
+
         function addZero(num) {
             if (num>=0 && num <10) {
                 return '0' + num;
             } else {
                 return num;
+            }
+        }
+
+        function text(num) {
+            let count = num % 10;
+            if (count == 1) {
+                return num + ' день';
+            } if (count>1 && count <5) {
+                return num + ' дня';
+            } else {
+                return num + ' дней';
             }
         }
     }
